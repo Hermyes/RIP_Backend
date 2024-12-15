7272befba64821adfe39005c847259fe8aaec3c1
Hermyes <aslan.ergaliev22@mail.ru>
2024-12-15 22:13:55 +0300

    Небольшие изменения cors для tauri билдов

--- lab3/lab3/settings.py
+++ lab3/lab3/settings.py
@@ -40,20 +40,30 @@ INSTALLED_APPS = [
     'rest_framework',
     'stocks',
     'drf_yasg',
+    "corsheaders",
+
 ]
 
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://tauri.localhost", 
+]
+
+# CORS_ALLOW_CREDENTIALS = True
 
 
 REST_FRAMEWORK = {
